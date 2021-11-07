bfded7c9c10a8943e248fddeadc066a4ab67454e
Lewis Barnes <lb.barnes@live.co.uk>
2021-11-07 17:44:17 +0000

    Added day 3 files.

---
+++ src/day-3/dayThree.ts
@@ -0,0 +1,30 @@
+import path from "path";
+
+import { readTextFile } from "../helpers/fileSystem";
+import { runWhenUsingCommandLine } from "../helpers/execution";
+import { getInputLines } from "../helpers/input";
+
+interface LineFragments {
+  range: [number, number];
+  letter: string;
+  password: string;
+}
+
+const parseInput = (input: string) => {
+  return null;
+}
+
+export const calculatePartOne = (input: string) => {
+  return parseInput(input);
+}
+
+export const calculatePartTwo = (input: string) => {
+  return parseInput(input);
+}
+
+runWhenUsingCommandLine(() => {
+  const input = readTextFile(path.join(__dirname, "input.txt"));
+
+  console.log("Part 1:", calculatePartOne(input));
+  console.log("Part 2:", calculatePartTwo(input));
+});
