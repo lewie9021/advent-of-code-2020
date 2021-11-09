8247a2c30c04607f61d95b504d1959934b1a046f
Lewis Barnes <lb.barnes@live.co.uk>
2021-11-09 11:59:09 +0000

    Completed day 3 - part 2.

--- src/day-3/dayThree.ts
+++ src/day-3/dayThree.ts
@@ -3,6 +3,7 @@ import path from "path";
 import { readTextFile } from "../helpers/fileSystem";
 import { runWhenUsingCommandLine } from "../helpers/execution";
 import { getInputLines } from "../helpers/input";
+import { multiply } from "../helpers/arrays";
 
 const parseInput = (input: string) => {
   const grid = getInputLines(input)
@@ -48,7 +49,30 @@ export const calculatePartOne = (input: string) => {
 }
 
 export const calculatePartTwo = (input: string) => {
-  return parseInput(input);
+  const grid = parseInput(input);
+  const { height } = grid.getDimensions();
+  const slopes = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+  ]
+    .map(([right, down]) => {
+      let encounters = 0;
+
+      for (let i = 0; i < height; i += down) {
+        const point = grid.getCoordinate((i / down) * right, i);
+
+        if (grid.isTree(point)) {
+          encounters += 1;
+        }
+      }
+
+      return encounters
+    });
+
+  return multiply(slopes);
 }
 
 runWhenUsingCommandLine(() => {
