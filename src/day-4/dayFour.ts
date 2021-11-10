f0ffbe7b4d3287daed3cef4417d643dd1adf6091
Lewis Barnes <lb.barnes@live.co.uk>
2021-11-10 08:48:16 +0000

    Completed day 4 - part 2.

--- src/day-4/dayFour.ts
+++ src/day-4/dayFour.ts
@@ -20,6 +20,10 @@ const parseInput = (input: string) => {
     });
 }
 
+const between = (value: number, min: number, max: number) => {
+  return value >= min && value <= max;
+};
+
 export const calculatePartOne = (input: string) => {
   const passports = parseInput(input);
   const requiredFields = [
@@ -34,13 +38,80 @@ export const calculatePartOne = (input: string) => {
 
   return passports.filter((pairs) => {
     return requiredFields.every((field) => {
-      return Boolean(pairs.find((x) => x.field === field));
+      const match = pairs.find((x) => x.field === field);
+
+      return Boolean(match);
     });
   }).length;
 }
 
 export const calculatePartTwo = (input: string) => {
-  return parseInput(input)
+  const passports = parseInput(input);
+  const validation = [
+    {
+      field: "byr", // Birth Year
+      validate: (value: string) => {
+        return between(parseInt(value), 1920, 2002);
+      }
+    },
+    {
+      field: "iyr", // Issue Year
+      validate: (value: string) => {
+        return between(parseInt(value), 2010, 2020);
+      }
+    },
+    {
+      field: "eyr", // Expiration Year
+      validate: (value: string) => {
+        return between(parseInt(value), 2020, 2030);
+      }
+    },
+    {
+      field: "hgt", // Height
+      validate: (value: string) => {
+        const match = value.match(/^(\d+)(cm|in)$/);
+
+        if (!match) {
+          return false;
+        }
+
+        const height = parseInt(match[1]);
+        const measurement = match[2];
+
+        switch (measurement) {
+          case "cm": return between(height, 150, 193);
+          case "in": return between(height, 59, 76);
+          default: return false;
+        }
+      }
+    },
+    {
+      field: "hcl", // Hair Color
+      validate: (value: string) => {
+        return /^#[a-f|0-9]{6}$/.test(value);
+      }
+    },
+    {
+      field: "ecl", // Eye Color
+      validate: (value: string) => {
+        return ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(value);
+      }
+    },
+    {
+      field: "pid", // Passport ID
+      validate: (value: string) => {
+        return /^[0-9]{9}$/.test(value);
+      }
+    }
+  ];
+
+  return passports.filter((pairs) => {
+    return validation.every(({ field, validate }) => {
+      const match = pairs.find((x) => x.field === field && validate(x.value));
+
+      return Boolean(match);
+    });
+  }).length;
 }
 
 runWhenUsingCommandLine(() => {
