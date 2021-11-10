8af7d078998ca43d9a6963ff1cd074ad800e71e4
Lewis Barnes <lb.barnes@live.co.uk>
2021-11-10 07:39:38 +0000

    Added template files.

---
+++ __template__/__tests__/dayX.unit.ts
@@ -0,0 +1,18 @@
+import os from "os";
+import { calculatePartOne, calculatePartTwo } from "../dayX";
+
+describe("Part 1", () => {
+  it("returns X given the example input", () => {
+    const input = [].join(os.EOL);
+
+    expect(calculatePartOne(input)).toEqual(null);
+  });
+});
+
+describe("Part 2", () => {
+  it("returns X given the example input", () => {
+    const input = [].join(os.EOL);
+
+    expect(calculatePartTwo(input)).toEqual(null);
+  });
+});
