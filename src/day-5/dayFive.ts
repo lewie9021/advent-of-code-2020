526c3af3e2a8c6ac13e2424611868f1e2490c03c
Lewis Barnes <lb.barnes@live.co.uk>
2021-11-10 14:40:01 +0000

    Completed day 5 - part 1.

--- src/day-5/dayFive.ts
+++ src/day-5/dayFive.ts
@@ -2,13 +2,45 @@ import path from "path";
 
 import { readTextFile } from "../helpers/fileSystem";
 import { runWhenUsingCommandLine } from "../helpers/execution";
+import { getInputLines } from "../helpers/input";
 
 const parseInput = (input: string) => {
-  return null;
+  return getInputLines(input);
 }
 
 export const calculatePartOne = (input: string) => {
-  return parseInput(input)
+  const passes = parseInput(input)
+    .map((line) => {
+      const row = line
+        .substr(0, 7)
+        .split("")
+        .reduce((res, char) => {
+          const diff = res[1] - res[0];
+
+          switch (char) {
+            case "B": return [res[0] + Math.round(diff / 2), res[1]];
+            case "F": return [res[0], res[1] - Math.round(diff / 2)];
+          }
+        }, [0, 127])[0];
+      const column = line.substr(7)
+        .split("")
+        .reduce((res, char) => {
+          const diff = res[1] - res[0];
+
+          switch (char) {
+            case "R": return [res[0] + Math.round(diff / 2), res[1]];
+            case "L": return [res[0], res[1] - Math.round(diff / 2)];
+          }
+        }, [0, 7])[0];
+
+      return {
+        id: row * 8 + column,
+        row,
+        column
+      };
+    });
+
+  return passes.reduce((res, pass) => Math.max(res, pass.id), 0);
 }
 
 export const calculatePartTwo = (input: string) => {
